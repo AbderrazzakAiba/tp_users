90cc2771329254d6146cbcb3ed139237108d74f1
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-12-09 22:52:43 +0100

    green:test UpdateUserUseCase  should use output port instead of return

--- src/usecase/update_user_usecase.py
+++ src/usecase/update_user_usecase.py
@@ -2,15 +2,19 @@ from src.usecase.update_user_response import UpdateUserResponse
 from src.usecase.validators import is_valid_name
 
 class UpdateUserUseCase:
-    def __init__(self, repo):
+    def __init__(self, repo, output):
         self.repo = repo
+        self.output = output
 
     def execute(self, request):
         if not is_valid_name(request.name):
-            return UpdateUserResponse(False)
+            response = UpdateUserResponse(False)
+            self.output.present(response)
+            return
 
         updated = self._update_user(request.email, request.name)
-        return UpdateUserResponse(updated)
+        response = UpdateUserResponse(updated)
+        self.output.present(response)
 
     def _update_user(self, email, name):
         return self.repo.update(email, name)
