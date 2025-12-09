fbf7f16f2ddec443977c808b23cfc99349f3bf71
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-12-09 22:12:17 +0100

    blue:test delete non existing user should return deleted false

--- src/usecase/delete_user_usecase.py
+++ src/usecase/delete_user_usecase.py
@@ -5,5 +5,8 @@ class DeleteUserUseCase:
         self.repo = repo
 
     def execute(self, request):
-        deleted = self.repo.delete_by_email(request.email)
+        deleted = self._delete_user(request.email)
         return DeleteUserResponse(deleted)
+
+    def _delete_user(self, email):
+        return self.repo.delete_by_email(email)
