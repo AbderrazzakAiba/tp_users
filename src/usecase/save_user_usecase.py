4ed97f9752f23335184595047c02d4d8f367cb83
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-12-09 22:38:25 +0100

    green:test save user should send response to output port

--- src/usecase/save_user_usecase.py
+++ src/usecase/save_user_usecase.py
@@ -4,19 +4,25 @@ from src.usecase.validators import is_valid_email, is_valid_name
 
 
 class SaveUserUseCase:
-    def __init__(self, repo):
+    def __init__(self, repo, output):
         self.repo = repo
+        self.output = output
 
     def execute(self, request):
         if not self._is_valid_request(request):
-            return SaveUserResponse(False)
+            response = SaveUserResponse(False)
+            self.output.present(response)
+            return
 
         if self._email_exists(request.email):
-            return SaveUserResponse(False)
+            response = SaveUserResponse(False)
+            self.output.present(response)
+            return
 
         user = User(request.email, request.name)
         self.repo.save(user)
-        return SaveUserResponse(True)
+        response = SaveUserResponse(True)
+        self.output.present(response)
 
     def _is_valid_request(self, request):
         if not is_valid_email(request.email):
@@ -28,6 +34,4 @@ class SaveUserUseCase:
         return True
 
     def _email_exists(self, email):
-        if hasattr(self.repo, "exists_by_email"):
-            return self.repo.exists_by_email(email)
-        return False
+        return self.repo.exists_by_email(email)
