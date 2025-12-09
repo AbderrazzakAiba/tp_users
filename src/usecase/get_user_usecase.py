a1a6ce1817357f1028f7bcc383338abf79c66a74
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-12-09 21:55:14 +0100

    blue:test get  user should return found false

--- src/usecase/get_user_usecase.py
+++ src/usecase/get_user_usecase.py
@@ -5,7 +5,10 @@ class GetUserUseCase:
         self.repo = repo
 
     def execute(self, request):
-        user = self.repo.find_by_email(request.email)
+        user = self._find_user(request.email)
         if user:
             return GetUserResponse(True, user)
         return GetUserResponse(False, None)
+
+    def _find_user(self, email):
+        return self.repo.find_by_email(email)
