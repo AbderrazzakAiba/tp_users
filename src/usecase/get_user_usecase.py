7077e81e486efde23e180f0eaffb12c7c1693737
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-12-09 22:45:36 +0100

    green:test GetUserUseCase should use output port instead of return

--- src/usecase/get_user_usecase.py
+++ src/usecase/get_user_usecase.py
@@ -1,14 +1,19 @@
 from src.usecase.get_user_response import GetUserResponse
 
 class GetUserUseCase:
-    def __init__(self, repo):
+    def __init__(self, repo, output):
         self.repo = repo
+        self.output = output
 
     def execute(self, request):
         user = self._find_user(request.email)
         if user:
-            return GetUserResponse(True, user)
-        return GetUserResponse(False, None)
+            response = GetUserResponse(True, user)
+            self.output.present(response)
+            return
+
+        response = GetUserResponse(False, None)
+        self.output.present(response)
 
     def _find_user(self, email):
         return self.repo.find_by_email(email)
