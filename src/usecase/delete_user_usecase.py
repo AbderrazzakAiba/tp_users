8b5f58f2a1003a5baec501659529f5e213d06094
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-12-09 22:50:17 +0100

    green:test DeleteUserUseCase should use output port instead of return

--- src/usecase/delete_user_usecase.py
+++ src/usecase/delete_user_usecase.py
@@ -1,12 +1,14 @@
 from src.usecase.delete_user_response import DeleteUserResponse
 
 class DeleteUserUseCase:
-    def __init__(self, repo):
+    def __init__(self, repo, output):
         self.repo = repo
+        self.output = output
 
     def execute(self, request):
         deleted = self._delete_user(request.email)
-        return DeleteUserResponse(deleted)
+        response = DeleteUserResponse(deleted)
+        self.output.present(response)
 
     def _delete_user(self, email):
         return self.repo.delete_by_email(email)
