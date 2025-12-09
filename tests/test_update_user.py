b0f0e981da553f989d540f4b102c31836af2a2b7
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-12-09 22:16:45 +0100

    red:test update existing user should modify its data

---
+++ tests/test_update_user.py
@@ -0,0 +1,32 @@
+from src.usecase.update_user_usecase import UpdateUserUseCase
+from src.usecase.update_user_request import UpdateUserRequest
+
+
+class InMemoryUserRepositoryFake:
+    def __init__(self):
+        self.saved = []
+
+    def save(self, user):
+        self.saved.append(user)
+
+    def update(self, email, name):
+        for user in self.saved:
+            if user.email == email:
+                user.name = name
+                return True
+        return False
+
+
+def test_update_existing_user():
+    repo = InMemoryUserRepositoryFake()
+
+    repo.save(type('User', (), {'email': 'alex@example.com', 'name': 'Alex'})())
+
+    usecase = UpdateUserUseCase(repo)
+
+    request = UpdateUserRequest('alex@example.com', 'Alicia')
+
+    response = usecase.execute(request)
+
+    assert response.updated is True
+    assert repo.saved[0].name == 'Alicia'
