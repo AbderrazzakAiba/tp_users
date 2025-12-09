8d8077e8a73a77229f9e914db5b5a87cc9d51e2a
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-12-09 22:27:38 +0100

    green:test updating user with invalid name should fail

--- src/usecase/update_user_usecase.py
+++ src/usecase/update_user_usecase.py
@@ -1,10 +1,14 @@
 from src.usecase.update_user_response import UpdateUserResponse
+from src.usecase.validators import is_valid_name
 
 class UpdateUserUseCase:
     def __init__(self, repo):
         self.repo = repo
 
     def execute(self, request):
+        if not is_valid_name(request.name):
+            return UpdateUserResponse(False)
+
         updated = self._update_user(request.email, request.name)
         return UpdateUserResponse(updated)
 
